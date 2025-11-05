762b951287cf758531f59da30bd723bde8985161
YJX <jianxiang.yang@yz-intelligence.com>
2025-11-05 12:29:35 +0800

    bug test

--- src/test/java/testcases/Test001.java
+++ src/test/java/testcases/Test001.java
@@ -11,9 +11,9 @@ public class Test001 {
 			assertEquals(3, FloorSquareRoot.floorSqrt(11));
 	 }
 
-	 @Test
-	 public void testB() {
-			assertEquals(4, FloorSquareRoot.floorSqrt(20));
-	 }
+	@Test
+	public void testB() {
+		assertEquals(5, FloorSquareRoot.floorSqrt(20));
+	}
 }
 
